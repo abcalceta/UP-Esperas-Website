37a63a44af2a6b10b3caa87351ea0f2df254e5e6
Carl Dizon <atomsandmolecules@msn.com>
2019-10-18 09:20:52 +0800

    Migrating FJK site to Mithril.js

---
+++ microsites/fjk2019/templates/footer.jsx
@@ -0,0 +1,38 @@
+<div class="footer-main container">
+    <div class="row">
+        <div class="col s12 m8 black-text">
+            <h5>Contact</h5>
+            <div class="row">
+                <div class="col s1">
+                    <i class="material-icons">email</i>
+                </div>
+                <div class="col s8">
+                    <a href="mailto:peyc2020@gmail.com" target="_blank">
+                        peyc<div class="hide">you</div>2020<div class="hide">have</div>@<div class="hide">been</div>gmail<div class="hide">pwned</div>.<div class="hide">here</div>com
+                    </a>
+                    <a href="mailto:esperanto.philippines@gmail.com" target="_blank">
+                        esper<div class="hide">saluton</div>anto<div class="hide">kiel</div>.<div class="hide">vi</div>philippines<div class="hide">fartas</div>@<div class="hide">bone</div>gmail<div class="hide">koran</div>.<div class="hide">dankon</div>com
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s1">
+                    <i class="material-icons">phone</i>
+                </div>
+                <div class="col s8">
+                    <a href="tel:+639982491891">
+                        +63<div class="hide">908</div>
+                        998<div class="hide">1241<br>+63 919</div>
+                        249<div class="hide">1640<br>+63 908</div>
+                        1891<div class="hide">110</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer-copyright green darken-4">
+    <div class="container grey-text text-lighten-5">
+        &copy; 2019 First Philippine Esperanto Youth Congress
+    </div>
+</div>
---
+++ microsites/fjk2019/templates/index.jsx
@@ -0,0 +1,198 @@
+<section id="section-about" class="section scrollspy">
+    <h3>About</h3>
+    <p>
+        The <em>Philippine Esperanto Youth</em> (FEJ), in cooperation with the
+        <em>Esperanto Society of the University of the Philippines</em>
+        (UP Esperanto-Societo/UP Esperas) will be organizing the first ever
+        <b>Philippine Esperanto Youth Conference</b> (PEYC), which will
+        take place in 2020 in Manila, Philippines.
+    </p>
+    <p>
+        This historic congress shall mark the culmination of the seven years of
+        effort of Esperanto advocates in the country and the forward step towards
+        the further growth and integration of the national Esperanto community,
+        and shall provide speakers, learners, and advocates coming from different
+        parts of the country the opportunity to interact, learn, and work together
+        in a series of social and educational activities.
+    </p>
+    <p>
+        With the theme <em>The Esperanto Movement: fresh start in a multicultural Philippines</em>,
+        the congress will serve a unique and grand role in convening the Esperanto youth
+        for the first time across the country. Aside from sessions on the Philippine Esperanto
+        movement, there will also be cultural talks, an Esperanto crash course, fun social
+        activities, and an excursion to some historical or natural parts around Manila.
+    </p>
+</section>
+
+<section id="section-why-congress" class="section scrollspy">
+    <h3>Why Hold a Congress?</h3>
+    <p>
+        The Congress, a major milestone for the local Esperanto movement, is actually
+        highly anticipated in the international Esperanto community. Already long overdue,
+        it is expected to draw the attendance of speakers and advocates of the language
+        from other countries, most of which aim to tour the country and get to know with
+        members of the local Esperanto community, providing a truly international experience
+        for Filipinos without leaving the country.
+    </p>
+    <p>
+        This Congress will also be the most appropriate opportunity to consolidate the
+        Esperanto movement in the Philippines and bring together local speakers and advocates
+        from all regions of the country. This will be avenues for everyone, coming from diverse
+        fields of interests and expertise, to freely socialize and exchange information and
+        opinions on a wide range of topics. Most important, lasting initiatives for the local
+        Esperanto movement may also be born here.
+    </p>
+    <p>
+        Furthermore, if successfully organized, the Congress shall be an impetus towards a
+        wider public and institutional knowledge and recognition of Esperanto as a living
+        language for egalitarian interethnic communication, eventually drawing the interest
+        of many people from different walks of life to learn the language and be part of the
+        national Esperanto community.
+    </p>
+</section>
+
+<section id="section-lkk" class="section scrollspy">
+    <h3>Local Organizing Committee</h3>
+    <div class="row">
+        <div class="col s12 m3">
+            <img class="responsive-img circle" alt="Albert Garrido" width="200" src="img/p1_profile.jpg">
+        </div>
+        <div class="col s12 m9">
+            <h5 class="promo-header">Albert Garrido</h5>
+            <div class="chip yellow darken-1">Organizing Committee President</div>
+            <div class="chip yellow darken-1">Director for Volunteer Staff</div>
+            <p>
+                Albert started learning Esperanto in March 2013 and soon afterwards started to get
+                involved in the Esperanto movement in the local and international level. He is
+                currently the Coordinator of Asia of the World Esperanto Youth Organization (TEJO),
+                and has attended a few international Esperanto conventions in some countries in Asia.
+                He also has a bachelor’s degree in Geography and wrote his undergraduate thesis
+                examining the state of the Asian Esperanto movement.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 m3">
+            <img class="responsive-img circle" alt="Carl Dizon" width="200" src="img/p2_profile.jpg">
+        </div>
+        <div class="col s12 m9">
+            <h5 class="promo-header">Carl Dizon</h5>
+            <div class="chip yellow darken-1">Director for Registration</div>
+            <p>
+                Carl is the incumbent president of UP Esperanto-Societo, the first and
+                only university-based Esperanto organization in the country. He is currently
+                the head researcher of an e-trike study and is taking his Masters
+                in Electrical Engineering at the University of the Philippines.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 m3">
+            <img class="responsive-img circle" alt="Jubert Cabrezos" width="200" src="img/p3_profile.jpg">
+        </div>
+        <div class="col s12 m9">
+            <h5 class="promo-header">Jubert Cabrezos</h5>
+            <div class="chip yellow darken-1">Director for Information and Publicity</div>
+            <p>
+                Jubert is a poet, artist, and Esperantist from Medina, Misamis Oriental.
+                He earned his Bachelor of Arts degree in Communication Arts at the Philippine
+                Women’s University. He worked in the National Secretariat of the College Editors
+                Guild of the Philippines for over 3 years. He currently lives in North Caloocan
+                with his family.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 m3">
+            <img class="responsive-img circle" alt="Elizabeth Lavadia" width="200" src="img/p4_profile.jpg">
+        </div>
+        <div class="col s12 m9">
+            <h5 class="promo-header">Elizabeth Lavadia</h5>
+            <div class="chip yellow darken-1">Director for External Relations</div>
+            <p>
+                Elizabeth is an instructor for the Italian language at the University
+                of the Philippines, Diliman. She took a C2 level short course on the Italian
+                Language and Culture at Università per Stranieri di Siena from April to June 2019.
+                She graduated Magna cum Laude in 2018 and has a Bachelor of Arts degree in European
+                Languages from the University of the Philippines, Diliman.
+            </p>
+        </div>
+    </div>
+</section>
+
+<section id="section-orgs" class="section scrollspy">
+    <h3>Participating Organizations</h3>
+
+    <h4>Organizers</h4>
+    <div class="row">
+        <div class="col s12 m6 center-align">
+            <div><img alt="FEJ logo" src="img/logo_fej.png" width="100"></div>
+            <span class="promo-header">Philippine Esperanto Youth</span>
+        </div>
+        <div class="col s12 m6 center-align">
+            <div><img alt="UP Esperas logo" src="img/logo_esperas.png" width="100"></div>
+            <span class="promo-header">Esperanto Society of the University of the Philippines</span>
+        </div>
+    </div>
+</section>
+
+<section id="section-how-to-help" class="scrollspy">
+    <h3>How Can You Help?</h3>
+    <p>
+        As the first-ever large-scale Esperanto congress in the country, you can
+        contribute to the realization of this event through different means.
+    </p>
+    <div class="row">
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/register_unsplash.jpg">
+                    <span class="card-title">Join the LKK</span>
+                </div>
+                <div class="card-content">
+                    <p>
+                        You can still be part of the local organizing committee of
+                        PEYC 2020.
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a href="call_for_organizers.html" class="green-text text-darken-4">How to Apply</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/eo_books_flower.jpg">
+                    <span class="card-title">Volunteer</span>
+                </div>
+                <div class="card-content">
+                    <p>
+                        The current local organizing committee of PEYC 2020 is in need of
+                        people who want to be part of the Volunteer Staff!
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a href="call_for_volunteers.html" class="green-text text-darken-4">How to Apply</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/donate_unsplash.jpg">
+                    <span class="card-title">Donate</span>
+                </div>
+                <div class="card-content">
+                    <p>
+                        You can also opt to support this congress financially or logistically through
+                        different means.
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a href="donate.html" class="green-text text-darken-4">Help Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
