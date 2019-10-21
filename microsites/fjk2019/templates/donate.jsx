43dfeecd86ebc6954e3317bd792446985ae8b7f0
Carl Dizon <atomsandmolecules@msn.com>
2019-10-21 09:25:49 +0800

    Decoupling reusable components in FJK website

---
+++ microsites/fjk2019/templates/donate.jsx
@@ -0,0 +1,115 @@
+<section class="section">
+    <h3>We need your help!</h3>
+    <p>
+        The Philippine Esperanto Youth (FEJ) is pleased to announce, that for the first time
+        in the history of the movement of speakers, learners, and advocates of the international
+        language Esperanto in the country, the 1st Philippine Esperanto Youth Congress will
+        be held in Manila on April 23-26, 2020.
+    </p>
+    <p>
+        To guarantee the success of this congress, the Congress Organizing Committee are
+        requesting everyone to support this initiative financially or through material support.
+        Through your donation, you will help give Filipino Esperanto speakers and learners
+        the opportunity to experience a truly multicultural atmosphere, consolidate the
+        Esperanto movement in the Philippines, and ensure the continuous growth of the local
+        community.
+    </p>
+</section>
+<section class="section">
+    <h3>The Congress Fund</h3>
+    <p>
+        This fund will be used to finance the whole congress, including venue rentals,
+        accommodation, and others. A proposed budget plan will be released immediately
+        once the LKK has approved it.
+    </p>
+    <p>
+        Eventually, your contributions to the congress fund will generally
+    </p>
+
+    <ul class="browser-default">
+        <li>finance and implement important parts of the event</li>
+        <li>keep the registration fees low enough for young participants</li>
+        <li>fund the succeeding activities of Philippine Esperanto Youth, in case of a profit</li>
+    </ul>
+    <p>
+        Additionally, in case that we are able to collect enough, we will use part of the funds
+        to finance participants from distant regions, who may have difficulties in joining due
+        to economic reasons.
+    </p>
+</section>
+<section class="section">
+    <h3>Donation Options</h3>
+    <div class="row">
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/paypal_splash.jpg">
+                </div>
+                <div class="card-content">
+                    <p>
+                        You can donate directly to the congress funds via PayPal
+                        using your credit card.
+                    </p>
+                </div>
+                <div class="card-action">
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                        <input type="hidden" name="cmd" value="_s-xclick" />
+                        <input type="hidden" name="hosted_button_id" value="ZEGNAE7L6ESB6" />
+                        <button class="waves-effect waves-green green-text text-darken-4 btn-flat">Donate</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/ggf_splash.jpg">
+                </div>
+                <div class="card-content">
+                    <p>
+                        You can also donate through GoGetFunding to see
+                        some progress regarding the congress funds.
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a class="waves-effect waves-green green-text text-darken-4 btn-flat" target="_blank" href="https://gogetfunding.com/help-make-the-1st-philippine-esperanto-youth-congress-happen/">Donate</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/co_splash.jpg">
+                </div>
+                <div class="card-content">
+                    <p>
+                        If you are a member of UEA, or prefer to donate through
+                        the Central Office, you can donate using the code
+                        <b>phej-d</b>.
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a class="waves-effect waves-green green-text text-darken-4 btn-flat" target="_blank" href="https://uea.org/alighoj/spagilo">Donate</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="img/eo_books_flower.jpg">
+                </div>
+                <div class="card-content">
+                    <p>
+                        You can also send brochures and pamphlets that can be handed out
+                        to congress participants.
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a href="mailto:peyc2020@gmail.com" class="waves-effect waves-green green-text text-darken-4 btn-flat">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ microsites/fjk2019/templates/navbar.jsx
@@ -0,0 +1,20 @@
+<!-- Navbar -->
+<nav class="z-depth-0">
+    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down scrollspy">
+        <li><a class="yellow-text"><b>Home</b></a></li>
+        <li><a class="dropdown-trigger" href="#" data-target="ul-nav-volunteer">Participate<i class="material-icons right">arrow_drop_down</i></a></li>
+        <li><a href="donate.html">Donate</a></li>
+    </ul>
+</nav>
+
+<ul id="ul-nav-volunteer" class="dropdown-content">
+    <li><a class="green-text waves-effect waves-light" href="call_for_organizers.html">Call for Organizers</a></li>
+    <li><a class="green-text waves-effect waves-light" href="call_for_volunteers.html">Call for Volunteers</a></li>
+</ul>
+
+<ul class="sidenav" id="mobile-demo">
+    <li><a class="green-text text-darken-4"><b>Home</b></a></li>
+    <li><a class="waves-effect waves-green" href="call_for_organizers.html">Call for Organizers</a></li>
+    <li><a class="waves-effect waves-green" href="call_for_volunteers.html">Call for Volunteers</a></li>
+</ul>
