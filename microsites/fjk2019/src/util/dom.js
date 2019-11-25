8314b6bb06a007d40fedcdc35ab609aaa4075838
Carl Dizon <atomsandmolecules@msn.com>
2019-11-26 02:32:58 +0800

    Completed UI of reg form in FJK site
    - Added logic for computing registration periods
    - Added some logic in registration categories

--- microsites/fjk2019/src/util/dom.js
+++ microsites/fjk2019/src/util/dom.js
@@ -4,21 +4,21 @@ function animate(targetsQuery, animateClasses, getParents = false, threshold = 0
     const intersectHandler = function intersectHandler(prevTop) {
         return function(entries) {
             entries.forEach(eachEntry => {
-                const classesList = ["animated"];
-                classesList.push(...animateClasses.split(" "));
+                const classesList = ['animated'];
+                classesList.push(...animateClasses.split(' '));
 
                 const nowTop = eachEntry.boundingClientRect.top;
                 const nowRatio = eachEntry.intersectionRatio;
 
                 if (eachEntry.isIntersecting && nowTop < prevTop && nowRatio > 0.5) {
                     //console.log(`fadeIn: ${nowTop} ${prevTop} ${nowRatio}`)
-                    eachEntry.target.classList.remove("fadeOut", "faster");
+                    eachEntry.target.classList.remove('fadeOut', 'faster');
                     eachEntry.target.classList.add(...classesList);
                 }
                 else if (!eachEntry.isIntersecting && nowTop > prevTop && nowRatio < 0.5) {
                     //console.log(`fadeOut: ${nowTop} ${prevTop} ${nowRatio}`)
                     eachEntry.target.classList.remove(...classesList);
-                    eachEntry.target.classList.add("animated", "fadeOut", "faster");
+                    eachEntry.target.classList.add('animated', 'fadeOut', 'faster');
                 }
 
                 prevTop = nowTop;
@@ -31,7 +31,7 @@ function animate(targetsQuery, animateClasses, getParents = false, threshold = 0
         const scrollObserver = new IntersectionObserver(
             intersectHandler(0),
             {
-                //rootMargin: "0% 0% -50% 0%",
+                //rootMargin: '0% 0% -50% 0%',
                 threshold: [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
             });
 
@@ -45,13 +45,15 @@ function animate(targetsQuery, animateClasses, getParents = false, threshold = 0
 }
 
 function initDomScripts() {
-    Materialize.Parallax.init(document.querySelectorAll(".parallax"));
-    Materialize.Sidenav.init(document.querySelectorAll(".sidenav"));
+    Materialize.Modal.init(document.querySelectorAll('.modal'));
+    Materialize.Parallax.init(document.querySelectorAll('.parallax'));
+    Materialize.Sidenav.init(document.querySelectorAll('.sidenav'));
     Materialize.ScrollSpy.init(document.querySelectorAll('.scrollspy'));
-    Materialize.Dropdown.init(document.querySelectorAll(".dropdown-trigger"), {hover: true});
-    Materialize.Collapsible.init(document.querySelectorAll(".collapsible"), {});
-    Materialize.FormSelect.init(document.querySelectorAll("select"));
-    Materialize.Datepicker.init(document.querySelectorAll(".datepicker"), {
+    Materialize.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {hover: true});
+    Materialize.Collapsible.init(document.querySelectorAll('.collapsible'), {});
+    Materialize.FormSelect.init(document.querySelectorAll('select'));
+    Materialize.Tooltip.init(document.querySelectorAll('.tooltipped'));
+    Materialize.Datepicker.init(document.querySelectorAll('.datepicker'), {
         autoClose: true,
         format: 'yyyy-mm-dd',
         yearRange: [1900, 2019]
--- microsites/fjk2019/src/views/RegisterPage.js
+++ microsites/fjk2019/src/views/RegisterPage.js
@@ -21,7 +21,7 @@ export class RegisterPage extends BasePage {
             }
         )
 
-
+        this.regIdxToName = ['early', 'regular', 'late'];
         this.componentHolder.main = htmlMain;
         this.countryList = [];
     }
@@ -40,8 +40,8 @@ export class RegisterPage extends BasePage {
         super.oninit();
     }
 
-    oncreate() {
-        console.log("here!")
+    onupdate() {
+        // Populate country list
         let countrySelectElm = document.querySelector("#select-countries-list");
 
         this.countryList.forEach((v) => {
@@ -52,23 +52,67 @@ export class RegisterPage extends BasePage {
             countrySelectElm.appendChild(optElm);
         });
 
+        // Check registration date
+        let regDateIdx = this.checkRegistrationDates();
+        let cardPanelElm = document.querySelector('#div-panel-reg-date-notice');
 
-        super.oncreate();
+        m.render(cardPanelElm, [
+            m('span', {class: 'white-text'}, [
+                'You are eligible for ',
+                m('b', `${this.regIdxToName[regDateIdx]}`),
+                ' registration!'
+            ])
+        ])
+
+        DomScripts.initDomScripts();
+        this.attachRegEvent();
+    }
+
+    attachRegEvent() {
+        let regTypeRadioList = document.querySelectorAll('input[type=radio][name=reg-broad-category]');
+
+        regTypeRadioList.forEach((v) => {
+            v.addEventListener('change', (e) => {
+                let changedValue = v.getAttribute('value');
+
+                if(!v.checked) {
+                    return;
+                }
+
+                console.log(`checked ${changedValue}`);
+
+                switch(changedValue) {
+                    case 'reg-broad-regular': {
+                        break;
+                    }
+                    case 'reg-broad-moral': {
+                        v.parentElement.parentElement.append("Moral participants are who will not participate in the congress itself, but want to support the congress financially. They shall be given due acknowledgment in the congress booklet.")
+                        break;
+                    }
+                    case 'reg-broad-patron': {
+                        break;
+                    }
+                    default:
+                        // Pass
+                }
+            });
+        });
     }
 
     checkRegistrationDates() {
         let earlyBirdDate = new Date('2019-12-31T11:59:59+08:00');
-        let regDate = new Date('2020-02-29T11:59:59+08:00');
+        let regDate = new Date('2020-03-23T11:59:59+08:00');
         let dateToday = new Date();
 
         if(dateToday <= earlyBirdDate) {
-            console.log("Early bird reg!");
+            return 0;
         }
         else if(dateToday <= regDate) {
-            console.log("Regular reg!");
+            return 1;
         }
         else {
             console.log("Late reg!");
+            return 2;
         }
     }
 }
