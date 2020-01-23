e7934f921bc5a624514453a6ca2ec488f303a406
Carl Dizon <atomsandmolecules@msn.com>
2020-01-24 00:11:37 +0800

    Renamed FJK site folder
    - Added more translations and image in home page

--- microsites/fjk2020/src/web/views/IndexPage.js
+++ microsites/fjk2020/src/web/views/IndexPage.js
@@ -4,6 +4,9 @@ import {DomScripts} from '../util/dom';
 import {BasePage} from './BasePage';
 import htmlMain from '../templates/main.html';
 import heroPath from '../img/hero/banderitas_home_banner.jpg';
+import announceImgEn from '../img/hero/fjk_tejo_en.jpg';
+import announceImgEo from '../img/hero/fjk_tejo_eo.jpg';
+
 import '../styles/default.css';
 
 export class IndexPage extends BasePage {
@@ -19,6 +22,15 @@ export class IndexPage extends BasePage {
 
     onupdate() {
         super.onupdate();
+
+        const announceImg = document.getElementById('img-announce');
+
+        if(this.data.locale.lang == 'eo') {
+            announceImg.src = announceImgEo;
+        }
+        else {
+            announceImg.src = announceImgEn;
+        }
 
         DomScripts.animate('#section-how-to-help .card', 'zoomIn', true);
     }
